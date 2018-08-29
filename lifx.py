8753e997ba5fbfe00392663cd8d456ee80c3fa1a
John <18238836+czeque@users.noreply.github.com>
2018-08-29 18:19:42 -0400

    Update for testing

--- lifx.py
+++ lifx.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource, reqparse
 
 import sys
 from time import sleep
-from lifxlan import LifxLAN
+from lifxlan import LifxLAN, BLUE, GREEN
 
 app = Flask(__name__)
 api = Api(app)
